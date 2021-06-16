922760fd8ae434a167ad617836a124a990d5f94f
luffah <luffah@users.noreply.github.com>
2021-06-16 22:20:52 +0200

    rename project nextcloud-api-wrapper to publish on PyPi

--- setup.py
+++ setup.py
@@ -18,7 +18,8 @@ from setuptools import setup, find_packages
 # 'setup.py publish' shortcut.
 if sys.argv[-1] == 'publish':
     # see https://twine.readthedocs.io/en/latest/
-    os.system('%s %s sdist bdist_wheel' % (sys.executable, sys.argv[0]))
+    os.system('python %s sdist bdist_wheel' % (sys.argv[0]))
+    os.system('python3 %s sdist bdist_wheel' % (sys.argv[0]))
     os.system('twine upload dist/*')
     sys.exit()
 
