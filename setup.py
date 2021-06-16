0611e0e36e532af8f89a6115f4f72616cd044eae
luffah <luffah@users.noreply.github.com>
2021-06-16 23:08:12 +0200

    Update setup.py to publish properly on PyPi

--- setup.py
+++ setup.py
@@ -18,6 +18,7 @@ from setuptools import setup, find_packages
 # 'setup.py publish' shortcut.
 if sys.argv[-1] == 'publish':
     # see https://twine.readthedocs.io/en/latest/
+    os.system('rm -rf dist build')
     os.system('python %s sdist bdist_wheel' % (sys.argv[0]))
     os.system('python3 %s sdist bdist_wheel' % (sys.argv[0]))
     os.system('twine upload dist/*')
@@ -28,5 +29,5 @@ setup(
     # some variables are defined here for retro compat with setuptools >= 33
     package_dir = {'': 'src'},
     packages=find_packages(where=r'./src'),
-    long_description_content_type = 'text/markdown'
+    long_description_content_type = 'text/x-rst'
 )
