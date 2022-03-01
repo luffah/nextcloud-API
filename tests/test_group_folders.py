d61646b6decac8e0d8cc32dcb8c74a62c510733e
fangebee <fangebee@users.noreply.github.com>
2022-03-01 15:28:30 +0100

    Add tests grant/revoke advanced ACL to user/group

--- tests/test_group_folders.py
+++ tests/test_group_folders.py
@@ -137,7 +137,7 @@ class TestGroupFolders(BaseTestCase):
         # clear
         self.clear(nxc=self.nxc, group_ids=[group_id], group_folder_ids=[group_folder_id])
 
-    def test_manage_acl(self):
+    def test_grant_revoke_advanced_acl_to_user(self):
         # create group to share with
         group_id = 'test_folders_' + self.get_random_string(length=4)
         self.nxc.add_group(group_id)
@@ -157,25 +157,67 @@ class TestGroupFolders(BaseTestCase):
         res = self.nxc.get_group_folder(group_folder_id)
         assert int(res.data['quota']) == QUOTA_UNLIMITED
 
-        # define manager
+        # grant advanced ACL
         self.nxc.manage_acl(group_folder_id, username)
         # XXX We have to wait for commit https://github.com/nextcloud/groupfolders/commit/1c3874e0b980
         #res = self.nxc.get_group_folder(group_folder_id)
         # assert username in res.data["manage"]
-
         res = self.nxc.get_group_folders()
         assert str(group_folder_id) in res.data
         assert username in res.data[str(group_folder_id)]["manage"]
         assert res.data[str(group_folder_id)]["manage"][username]["type"] == "user"
 
-        # set permissions
-        new_permission = Permission.READ + Permission.CREATE
-        res = self.nxc.set_permissions_to_group_folder(group_folder_id, group_id, new_permission)
+        # revoke advanced ACL
+        self.nxc.manage_acl(group_folder_id, username, addacl=0)
+        # XXX We have to wait for commit https://github.com/nextcloud/groupfolders/commit/1c3874e0b980
+        #res = self.nxc.get_group_folder(group_folder_id)
+        # assert username not in res.data["manage"]
+        res = self.nxc.get_group_folders()
+        assert str(group_folder_id) in res.data
+        assert username not in res.data[str(group_folder_id)]["manage"]
+
+        # clear
+        self.clear(nxc=self.nxc, group_ids=[group_id], group_folder_ids=[group_folder_id])
+
+    def test_grant_revoke_advanced_acl_to_group(self):
+        # create group to share with
+        group_id = 'test_folders_' + self.get_random_string(length=4)
+        self.nxc.add_group(group_id)
+
+        # create a second group to manage advanced permissions
+        admin_group_id = 'admin_group_' + self.get_random_string(length=4)
+        self.nxc.add_group(admin_group_id)
+
+        # create new group folder
+        folder_mount_point = "test_folder_advanced_permissions_" + self.get_random_string(length=4)
+        res = self.nxc.create_group_folder(folder_mount_point)
         assert res.is_ok
-        assert res.data is True
-        # check if permissions changed
+        group_folder_id = res.data['id']
+
+        # add new group to folder
+        self.nxc.grant_access_to_group_folder(group_folder_id, group_id)
+        # assert permissions is ALL by default
         res = self.nxc.get_group_folder(group_folder_id)
-        assert str(res.data['groups'][group_id]) == str(new_permission)
+        assert int(res.data['quota']) == QUOTA_UNLIMITED
+
+        # grant advanced ACL
+        self.nxc.manage_acl(group_folder_id, admin_group_id, type_="group")
+        # XXX We have to wait for commit https://github.com/nextcloud/groupfolders/commit/1c3874e0b980
+        #res = self.nxc.get_group_folder(group_folder_id)
+        # assert admin_group_id in res.data["manage"]
+        res = self.nxc.get_group_folders()
+        assert str(group_folder_id) in res.data
+        assert admin_group_id in res.data[str(group_folder_id)]["manage"]
+        assert res.data[str(group_folder_id)]["manage"][admin_group_id]["type"] == "group"
+
+        # revoke advanced ACL
+        self.nxc.manage_acl(group_folder_id, admin_group_id, type_="group", addacl=0)
+        # XXX We have to wait for commit https://github.com/nextcloud/groupfolders/commit/1c3874e0b980
+        #res = self.nxc.get_group_folder(group_folder_id)
+        # assert admin_group_id not in res.data["manage"]
+        res = self.nxc.get_group_folders()
+        assert str(group_folder_id) in res.data
+        assert admin_group_id not in res.data[str(group_folder_id)]["manage"]
 
         # clear
         self.clear(nxc=self.nxc, group_ids=[group_id], group_folder_ids=[group_folder_id])
